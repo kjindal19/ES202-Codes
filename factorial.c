ad8e98f4807f8118d173be624742cd0e6e85b9d0
Kunal Jindal <kunal.devloper@gmail.com>
2021-12-07 18:14:48 +0530

    Initial commit

---
+++ factorial.c
@@ -0,0 +1,10 @@
+//
+// Created by Kunal Jindal on 07-12-2021.
+//
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, factorial!\n");
+    return 0;
+}
---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("My First C program in CLion\n");
+    printf("Now posting it to github");
+    return 0;
+}
