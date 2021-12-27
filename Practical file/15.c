d5c6d114625009d2ef90f7cb26368ccf79a77190
Kunal Jindal <kunal.devloper@gmail.com>
2021-12-28 01:57:07 +0530

    Adding programs in the Practical File

---
+++ Practical file/15.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    float bs, hra, da, gs;
+
+    printf("Enter basic salary\n");
+    scanf("%f", &bs);
+
+    hra = bs * (20/100.00);
+    da  = bs * (40/100.00);
+
+    gs  = bs + hra + da;
+
+    printf("Gross Salary = %f\n", gs);
+
+    return 0;
+}
